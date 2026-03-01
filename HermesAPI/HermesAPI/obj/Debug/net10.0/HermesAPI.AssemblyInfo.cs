87e0793a9d8cf1e3275fb63c1b70785b9fdc105a
Carlos Felipe Santos <117771889+CarlosFelipeS7@users.noreply.github.com>
2026-03-01 14:51:23 -0300

    FreteProfile

--- HermesAPI/HermesAPI/obj/Debug/net10.0/HermesAPI.AssemblyInfo.cs
+++ HermesAPI/HermesAPI/obj/Debug/net10.0/HermesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HermesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b4d551df5e42f3f4a65f91e6ee9b2a75cd17f3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe4a9a105dab0eddb69f6487c2ecff4d25688d2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HermesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HermesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
