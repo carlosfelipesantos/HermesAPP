cc789e2fcd8b1f26f430782154c5323123676ecf
Carlos Felipe Santos <117771889+CarlosFelipeS7@users.noreply.github.com>
2026-03-01 14:51:55 -0300

    AvaliacaoProfile

--- HermesAPI/HermesAPI/obj/Debug/net10.0/HermesAPI.AssemblyInfo.cs
+++ HermesAPI/HermesAPI/obj/Debug/net10.0/HermesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HermesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe4a9a105dab0eddb69f6487c2ecff4d25688d2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87e0793a9d8cf1e3275fb63c1b70785b9fdc105a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HermesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HermesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
