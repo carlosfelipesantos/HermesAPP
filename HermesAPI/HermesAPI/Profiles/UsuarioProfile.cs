1b4d551df5e42f3f4a65f91e6ee9b2a75cd17f3f
Carlos Felipe Santos <117771889+CarlosFelipeS7@users.noreply.github.com>
2026-03-01 14:50:07 -0300

    UsuarioProfile

---
+++ HermesAPI/HermesAPI/Profiles/UsuarioProfile.cs
@@ -0,0 +1,6 @@
+﻿namespace HermesAPI.Profiles
+{
+    public class UsuarioProfile
+    {
+    }
+}
--- HermesAPI/HermesAPI/obj/Debug/net10.0/HermesAPI.AssemblyInfo.cs
+++ HermesAPI/HermesAPI/obj/Debug/net10.0/HermesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HermesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a708db721cc7a57faaecaec8e4939b1e33861da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c05ff99dd4a891d3612c0a1f1b05feb2547006f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HermesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HermesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
