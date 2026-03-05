ec6a9bdc8173de7e75fe85a34f94b232d393d714
Carlos Felipe Santos <ccfelipe08@gmail.com>
2026-03-05 16:14:35 -0300

    Mapeando Usuario to UsuarioDTO

--- HermesAPI/HermesAPI/Profiles/UsuarioProfile.cs
+++ HermesAPI/HermesAPI/Profiles/UsuarioProfile.cs
@@ -1,6 +1,16 @@
-﻿namespace HermesAPI.Profiles
+﻿using AutoMapper;
+using HermesAPI.DTOs.UsuarioDTO;
+using HermesAPI.Entities;
+
+namespace HermesAPI.Profiles
 {
-    public class UsuarioProfile
+    public class UsuarioProfile : Profile
     {
+        public UsuarioProfile() //construtor da classe, onde é definido o mapeamento entre as classes
+        {
+            CreateMap<UsuarioProfile, UsuarioDTO>(); //mapeamento entre a classe UsuarioProfile e UsuarioDTO, onde os campos com o mesmo nome serão mapeados automaticamente
+            CreateMap<CriarUsuarioDTO, Usuario>(); //mapeamento entre a classe CriarUsuarioDTO e Usuario
+            CreateMap<AtualizarUsuarioDTO, Usuario>(); 
+        }
     }
 }
--- HermesAPI/HermesAPI/obj/Debug/net10.0/HermesAPI.AssemblyInfo.cs
+++ HermesAPI/HermesAPI/obj/Debug/net10.0/HermesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HermesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc789e2fcd8b1f26f430782154c5323123676ecf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f25937316261928be4b16d7322437e7821c06c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("HermesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HermesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
